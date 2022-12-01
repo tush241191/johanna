10975cb2d72517afa6f6ad80adef612f446db04a
Tushar Kharat <tushar.kharat@kwota.com>
2022-12-01 11:26:03 +0200

    responsive and snow effects

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-
 import Countdown from 'react-countdown';
+import Snowfall from 'react-snowfall'
+
 import Img from '../public/1.jpg'
 export default function Home() {
 
@@ -13,7 +14,7 @@ export default function Home() {
       // Render a countdown
       return( 
         <div className='text-center relative text-white'>
-          <div className='mt-2 grid grid-cols-4 gap-2'>
+          <div className='mt-2 flex flex-col md:grid md:grid-cols-4 gap-2'>
             <div className='w-40 flex flex-col p-2 border-4 border-sky-500 border-double'>
               <h1 className='text-4xl md:text-8xl'>{days}</h1>
               <span className='mt-2 text-blue-400'>Days</span>
@@ -38,11 +39,11 @@ export default function Home() {
   return (
     <div className={`w-full h-screen bg-black`}>
       <div className="relative h-full w-full">
+      <Snowfall/>
         <div className="flex h-full w-full">
           <div className="relative overflow-hidden h-full w-full">
             <div className="absolute inset-0 h-full w-full">
-              <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1579201157678-a242a244b34e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="People working on laptops" />
-              <div className="absolute inset-0 bg-indigo-700 mix-blend-multiply"></div>
+              <div className="absolute inset-0 bg-indigo-100 mix-blend-multiply"></div>
             </div>
             <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8 flex items-center justify-center h-full">
               <Countdown date="2022-12-05" renderer={renderer} />
